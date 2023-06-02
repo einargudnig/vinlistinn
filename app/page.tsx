02fd84c5242368db2777b5d0c2da8f7688a2a8c8
Einar <einargudnig@gmail.com>
2023-06-02 09:02:35 +0000

    adding shadcn ui 🤩

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { CardDemo } from '@/components/card'
 
 export default function Home() {
   return (
@@ -16,10 +17,7 @@ export default function Home() {
       </div>
       <div className="flex flex-row w-full">
       <div className="z-10 w-2/3 max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Scroll section
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
+        <CardDemo />
         
       </div>
         <div className="w-1/3">Wine Bottle</div>
