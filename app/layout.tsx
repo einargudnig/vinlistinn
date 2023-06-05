045beb25278922af139d8346395fbd3bdfb5028d
Einar <einargudnig@gmail.com>
2023-06-05 16:14:16 +0000

    favicon?

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Vínlistinn',
-  description: 'Fáðu vín frá litlum en frammúrskarandi vínbændum',
-}
+  title: "Vínlistinn",
+  description: "Fáðu vín frá litlum en frammúrskarandi vínbændum",
+  icon: "/favicon.ico",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
